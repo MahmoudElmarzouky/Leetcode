88e4c24ce729de039268884a691d3fcbde87f13d
Mahmoud Magdy <35300232+MahmoudElmarzouky@users.noreply.github.com>
2023-07-10 22:29:44 +0300

    Time: 139 ms (94.47%), Space: 111.6 MB (73.42%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+       int mn1=INT_MAX,mn2=INT_MAX;
+  for(int i=0;i<nums.size();++i){
+    if(nums[i]<=mn1)
+        mn1=nums[i];
+    else if(nums[i]<=mn2)
+        mn2=nums[i];
+    else return true;
+  }
+  return false;
+    }
+};
